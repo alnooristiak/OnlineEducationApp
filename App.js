2631f0f4c96ae5759558cc08deb927a7b02bdf35
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-11-09 20:33:24 +0600

    adding navagation and add to all pga in about.js file

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import About from './src/screen/About/About';
+import Contact from './src/screen/Contact/Contact';
+import Course from './src/screen/Course/Course';
 import Home from './src/screen/Home/Home';
+import UserDetail from './src/screen/UserDetail/UserDetail';
+// import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
+
+        {/* Home page  */}
         <Stack.Screen name="Home" component={Home} />
+
+        {/* About page  */}
+        <Stack.Screen name="About" component={About} />
+
+        {/* Contact us/Contact Page  */}
+        <Stack.Screen name="Contact" component={Contact} />
+
+        {/* Course details Page  */}
+        <Stack.Screen name="Course" component={Course} />
+
+        {/* User Detail Page  */}
+        <Stack.Screen name="UserDetail" component={UserDetail} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    // padding: 10,
-    marginTop: 40,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     // padding: 10,
+//     marginTop: 40,
+//   },
+// });
---
+++ src/screen/UserDetail/UserDetail.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UserDetail = () => {
+  return (
+    <View>
+      <Text>This is a UserDetail</Text>
+    </View>
+  )
+}
+
+export default UserDetail
+
+const styles = StyleSheet.create({})
